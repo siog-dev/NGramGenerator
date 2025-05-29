94a2ea54dc4a58a3c7719578c98a96ce6fa79ca5
siog <siog@LAPTOP-545TJ6PE>
2025-05-29 19:48:42 +0100

    Comment out current solution, add frame of given solution

--- NGramGenerator/src/ie/atu/sw/NGramGenerator.java
+++ NGramGenerator/src/ie/atu/sw/NGramGenerator.java
@@ -4,6 +4,22 @@ import java.util.Arrays;
 
 public class NGramGenerator {
 	
+	// JOHNS SOLUTION
+	public String[] getNGrams(String s, int n) {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		String s = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
+		
+		String[] ngrams = new NGramGenerator().getNGrams(s,  4);
+		for (String t : ngrams) {
+			System.out.println(t);
+		}
+	}
+	
+	// MY SOLUTION
+	/*
 	public String[] getNGrams(String s, int n) {
 		String[] result = new String[s.length() - n + 1];
 		for (int i = 0; i <= (s.length() - n); i++) {
@@ -18,4 +34,5 @@ public class NGramGenerator {
 		String[] s = ngg.getNGrams("HAPPY DAYS!", 4);
 		System.out.println(Arrays.toString(s));
 	}
+	*/
 }
