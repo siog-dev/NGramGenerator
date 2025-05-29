8dff28c10a82b2d8ea0fc4492350ebd44c04e5c5
siog <siog@LAPTOP-545TJ6PE>
2025-05-29 19:11:53 +0100

    Create a string array, loop over the array, get and set the nGrams

--- NGramGenerator/src/ie/atu/sw/NGramGenerator.java
+++ NGramGenerator/src/ie/atu/sw/NGramGenerator.java
@@ -1,5 +1,7 @@
 package ie.atu.sw;
 
+import java.util.Arrays;
+
 /*
  * An n-gram, also called a k-mer, is a subsequence of 
  * text of size n and can be used for a large variety of 
@@ -13,11 +15,27 @@ package ie.atu.sw;
 
 public class NGramGenerator {
 	
-	private String s = "HAPPY DAYS!";
-	private int n = 4;
+	// private String s = "HAPPY DAYS!";
+	// private int n = 4;
 	
 	public String[] getNGrams(String s, int n) {
-		return null;
+		/*
+		 * 1.	Create a new String array called result
+		 * 
+		 * 2.	Assign it the value of a new String[]
+		 * 
+		 * 3.	The amount of values (nGrams) in the array is 
+		 * 		the string length minus the length of the 
+		 * 		nGrams plus one (s.length() - n + 1)
+		 */
+		String[] result = new String[s.length() - n + 1];
+		// Loop over the array up to the last start position (s.length() - n)
+		for (int i = 0; i <= (s.length() - n); i++) {
+			// Get the nGrams from the array using substring, assign them to result
+			result[i] = s.substring(i, i+n);
+		}
+
+		return result;
 	}
 	
 	public static void main(String[] args) {
