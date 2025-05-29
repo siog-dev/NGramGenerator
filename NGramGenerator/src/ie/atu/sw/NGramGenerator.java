53efc52d690e1ea638d7443d7971fbb784ba200f
siog <siog@LAPTOP-545TJ6PE>
2025-05-29 17:18:21 +0100

    Comment-in the exercise question

--- NGramGenerator/src/ie/atu/sw/NGramGenerator.java
+++ NGramGenerator/src/ie/atu/sw/NGramGenerator.java
@@ -1,5 +1,16 @@
 package ie.atu.sw;
 
-public class NGramGenerator {
+/*
+ * An n-gram, also called a k-mer, is a subsequence of 
+ * text of size n and can be used for a large variety of 
+ * string processing applications. For example the 4-grams 
+ * of HAPPY DAYS! are HAPP, APPY, PPY_, PY_D... Create a 
+ * class called NGramGenerator that exposes a method with 
+ * the signature public String[] getNGrams(String s, int n) 
+ * that parses a String s and returns all the n-grams of 
+ * size n in a String array.
+ */
 
+public class NGramGenerator {
+	
 }
